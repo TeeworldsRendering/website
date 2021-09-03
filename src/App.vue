58a126323d57d3f58113adcd4fcf7126f9923015
theobori <theo1.bori@epitech.eu>
2021-09-03 14:41:20 +0200

    Base

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    Teeworlds skin renderer
+  </div>
+</template>
+
+<script>
+// Imports
+
+export default {
+  name: 'App',
+  components: {
+    // Compnents
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
